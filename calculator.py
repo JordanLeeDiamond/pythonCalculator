2878ce2007e152643475f52a3e0d4306908a1810
JordanLeeDiamond <JordanLeeDiamond@outlook.com>
2022-02-06 23:38:20 +0000

    second push :P

--- calculator.py
+++ calculator.py
@@ -1,1 +1,30 @@
-print ("hello world")
+# print ("hello world") has to be done :D
+
+# Simple Python Calculator
+
+# Add Function
+
+def add(x, y):
+    return x + y
+
+# Subtract Function
+
+def subtract(x, y):
+    return x - y
+
+# Multiply Function
+
+def multiply(x, y):
+    return x * y
+
+# Divide Function
+
+def divide(x, y):
+    return x / y
+
+
+print("Select a function")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
