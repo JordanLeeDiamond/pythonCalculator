f7c3aba3f7f4ed971a628415c6ae12251bd91cc3
JordanLeeDiamond <JordanLeeDiamond@outlook.com>
2022-02-07 22:02:30 +0000

    full basic python calculator

---
+++ basicCalcPY/calculator.py
@@ -0,0 +1,23 @@
+# print ("hello world") has to be done :D
+
+# Simple Python Calculator
+
+# Variables
+
+num1 = input("> ")
+num2 = input("> ")
+
+result = float(num1) + float(num2)
+
+print(result)
+
+
+# Use of "int" means you have to use whole number
+# Example:  num1 = input("> ")
+#           num2 = input("> ")
+
+#           result = int(num1) + int(num2)
+
+#           print(result)
+
+# Using "float" means you can use whole number and decimals.
---
+++ basicCalcPY/subtract.py
@@ -0,0 +1,6 @@
+num1 = input("> ")
+num2 = input("> ")
+
+result = float(num1) - float(num2)
+
+print(result)
